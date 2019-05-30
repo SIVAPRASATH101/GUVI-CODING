e3507c20c2a5efc94b8b55dbb9846755b3a436d7
SIVAPRASATH101 <43112006+SIVAPRASATH101@users.noreply.github.com>
2019-05-30 18:52:33 +0530

    Update print large word.py

--- test5/print large word.py
+++ test5/print large word.py
@@ -1,10 +1,5 @@
-l=[x for x in input().split()]
-a=len(l[0])
-b=len(l[1])
-if a==b :
-  print(l[0])
-elif a>b :
-  print(l[0])
-else :
-  print(l[1])
-  
+a,b = input().split()
+if (len(b) >= len(a)):
+	print(b)
+else:
+	print(a)
